c393c6b16ad793605bfd87ea7ec26bb26fb5eb1f
jaffar malladi <jaffar.iosappdev@gmail.com>
2022-07-26 15:57:50 +0530

    Initial Commit

---
+++ SerarchBarApp/SerarchBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerarchBarAppApp.swift
+//  SerarchBarApp
+//
+//  Created by Jaffar Malladi on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SerarchBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
